fa0a8c2cc17fc70d5859ad917842c17a3559eb93
cooldeark <cooldeark@gmail.com>
2024-06-07 03:10:08 -0400

    change sql to psql

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,23 +2,29 @@
 
 namespace Asantibanez\LaravelEloquentStateMachines\Tests;
 
-use CreateSalesManagersTable;
-use CreatePendingTransitionsTable;
-use CreateSalesOrdersTable;
-use CreateStateHistoriesTable;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Javoscript\MacroableModels\MacroableModelsServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use Asantibanez\LaravelEloquentStateMachines\LaravelEloquentStateMachinesServiceProvider;
 
 class TestCase extends BaseTestCase
 {
+    use RefreshDatabase, WithFaker;
+
     protected function setUp(): void
     {
         parent::setUp();
 
+        // 加载工厂
         $this->withFactories(__DIR__.'/../database/factories');
-
         $this->withFactories(__DIR__.'/database/factories');
+
+        // 运行迁移：使用 migrate:fresh 删除表并重新创建
+        $this->artisan('migrate:fresh', ['--database' => 'testing'])->run();
+
+        // 运行迁移
+        $this->runDatabaseMigrations();
     }
 
     protected function getPackageProviders($app)
@@ -31,15 +37,34 @@ class TestCase extends BaseTestCase
 
     protected function getEnvironmentSetUp($app)
     {
+        // 设置测试环境的数据库连接
+        $app['config']->set('database.default', 'testing');
+        $app['config']->set('database.connections.testing', [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'test_db'),
+            'username' => env('DB_USERNAME', 'trustanchor'),
+            'password' => env('DB_PASSWORD', 'Yai3hahMaepi9uyo3Joh'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+        ]);
+
+        // 包含迁移文件
         include_once __DIR__ . '/../database/migrations/create_state_histories_table.php.stub';
         include_once __DIR__ . '/../database/migrations/create_pending_transitions_table.php.stub';
-
         include_once __DIR__ . '/database/migrations/create_sales_orders_table.php';
         include_once __DIR__ . '/database/migrations/create_sales_managers_table.php';
+    }
 
-        (new CreateStateHistoriesTable())->up();
-        (new CreatePendingTransitionsTable())->up();
-        (new CreateSalesOrdersTable())->up();
-        (new CreateSalesManagersTable())->up();
+    protected function runDatabaseMigrations()
+    {
+        // 运行迁移
+        (new \CreateStateHistoriesTable())->up();
+        (new \CreatePendingTransitionsTable())->up();
+        (new \CreateSalesOrdersTable())->up();
+        (new \CreateSalesManagersTable())->up();
     }
 }
+
